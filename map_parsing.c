99fba3bb3a68e934412dfdcc5cb9292c67cbb8dd
byanis42 <byanis@student.42.fr>
2023-01-12 13:11:13 +0100

    Parsing finished typo

--- map_parsing.c
+++ map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: byanis <byanis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 14:18:42 by byanis            #+#    #+#             */
-/*   Updated: 2023/01/12 13:08:59 by byanis           ###   ########.fr       */
+/*   Updated: 2023/01/12 13:10:50 by byanis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ int	map_valid(char *map_string)
 int	parse_map(char *map_string)
 {
 	if (!map_is_rectangle(map_string) || !map_is_closed(map_string)
-		|| !map_valid(map_string) || !is_valid_path(map_string);)
+		|| !map_valid(map_string) || !is_valid_path(map_string))
 	{
 		ft_printf("Error\nMap isn't valid, retry with another map !\n");
 		return (0);
